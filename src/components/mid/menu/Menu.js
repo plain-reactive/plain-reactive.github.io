e5c1421228d81d01f2e96c69ce7623593ff713a6
Josep <jmartinbolet@gmail.com>
2024-07-17 12:47:51 +0200

    Updated

--- src/components/mid/menu/Menu.js
+++ src/components/mid/menu/Menu.js
@@ -10,15 +10,15 @@ class Menu extends PlainComponent {
 
     template() {
         return `
-            <p-menu-item class="item" number="1">Installing Plain</p-menu-item>
-            <p-menu-item class="item" number="2">Building your first component</p-menu-item>
-            <p-menu-item class="item" number="3">Setting up styles</p-menu-item>
-            <p-menu-item class="item" number="4">Adding state with PlainState</p-menu-item>
-            <p-menu-item class="item" number="5">Adding signals with PlainSignal</p-menu-item>
-            <p-menu-item class="item" number="6">Using contexts with PlainContext</p-menu-item>
-            <p-menu-item class="item" number="7">Using router with PlainRouter</p-menu-item>
-            <p-menu-item class="item" number="8">Dynamic rendering</p-menu-item>
-            <p-menu-item class="item" number="9">Rendering optimization and propagation</p-menu-item>
+            <p-menu-item class="item" number="01">Installing Plain</p-menu-item>
+            <p-menu-item class="item" number="02">Building your first component</p-menu-item>
+            <p-menu-item class="item" number="03">Setting up styles</p-menu-item>
+            <p-menu-item class="item" number="04">Adding state with PlainState</p-menu-item>
+            <p-menu-item class="item" number="05">Adding signals with PlainSignal</p-menu-item>
+            <p-menu-item class="item" number="06">Using contexts with PlainContext</p-menu-item>
+            <p-menu-item class="item" number="07">Using router with PlainRouter</p-menu-item>
+            <p-menu-item class="item" number="08">Dynamic rendering</p-menu-item>
+            <p-menu-item class="item" number="09">Rendering optimization and propagation</p-menu-item>
             <p-menu-item class="item" number="10">Lifting state up and props</p-menu-item>
             <p-menu-item class="item" number="11">Good practices</p-menu-item>
             <p-menu-item class="item" number="12">Example project</p-menu-item>
